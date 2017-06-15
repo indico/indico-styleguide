237732a59a74df7f897f132ce485f4c8dcfd8dfd
Ilias Trichopoulos <ilias.trichopoulos@cern.ch>
2017-06-15 11:06:45 +0200

    Fix indentation in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -21,50 +21,50 @@ const STATIC_BASE = '../static/';
 
 // configuration
 const config = {
-  dev: gutil.env.dev,
-  styles: {
-    browsers: 'last 1 version',
-    fabricator: {
-      src: 'src/assets/fabricator/styles/fabricator.scss',
-      dest: 'dist/assets/fabricator/styles',
-      watch: 'src/assets/fabricator/styles/**/*.scss',
+    dev: gutil.env.dev,
+    styles: {
+        browsers: 'last 1 version',
+        fabricator: {
+            src: 'src/assets/fabricator/styles/fabricator.scss',
+            dest: 'dist/assets/fabricator/styles',
+            watch: 'src/assets/fabricator/styles/**/*.scss',
+        },
+        toolkit: {
+            src: 'src/assets/toolkit/styles/toolkit.scss',
+            dest: 'dist/assets/toolkit/styles',
+            watch: 'src/assets/toolkit/styles/**/*.scss',
+        },
     },
-    toolkit: {
-      src: 'src/assets/toolkit/styles/toolkit.scss',
-      dest: 'dist/assets/toolkit/styles',
-      watch: 'src/assets/toolkit/styles/**/*.scss',
+    scripts: {
+        fabricator: {
+            src: './src/assets/fabricator/scripts/fabricator.js',
+            dest: 'dist/assets/fabricator/scripts',
+            watch: 'src/assets/fabricator/scripts/**/*',
+        },
+        toolkit: {
+            src: './src/assets/toolkit/scripts/toolkit.js',
+            dest: 'dist/assets/toolkit/scripts',
+            watch: 'src/assets/toolkit/scripts/**/*',
+        },
     },
-  },
-  scripts: {
-    fabricator: {
-      src: './src/assets/fabricator/scripts/fabricator.js',
-      dest: 'dist/assets/fabricator/scripts',
-      watch: 'src/assets/fabricator/scripts/**/*',
+    images: {
+        toolkit: {
+            src: ['src/assets/toolkit/images/*', 'src/favicon.ico'],
+            dest: 'dist/assets/toolkit/images',
+            watch: 'src/assets/toolkit/images/**/*',
+        },
     },
-    toolkit: {
-      src: './src/assets/toolkit/scripts/toolkit.js',
-      dest: 'dist/assets/toolkit/scripts',
-      watch: 'src/assets/toolkit/scripts/**/*',
+    fonts: {
+        indico: {
+            src: ['indico/indico/htdocs/static/fonts/**/*'],
+            dest: 'dist/assets/toolkit/static/fonts',
+            watch: 'indico/indico/htdocs/static/fonts/**/*',
+        },
     },
-  },
-  images: {
-    toolkit: {
-      src: ['src/assets/toolkit/images/*', 'src/favicon.ico'],
-      dest: 'dist/assets/toolkit/images',
-      watch: 'src/assets/toolkit/images/**/*',
+    templates: {
+        watch: 'src/**/*.{html,md,json,yml}',
     },
-  },
-  fonts: {
-    indico: {
-      src: ['indico/indico/htdocs/static/fonts/**/*'],
-      dest: 'dist/assets/toolkit/static/fonts',
-      watch: 'indico/indico/htdocs/static/fonts/**/*',
-    },
-  },
-  templates: {
-    watch: 'src/**/*.{html,md,json,yml}',
-  },
-  dest: 'dist',
+    dest: 'dist',
 };
 
 function reworkCSSUrls(style) {
@@ -125,74 +125,74 @@ gulp.task('styles', ['styles:fabricator', 'styles:toolkit']);
 const webpackConfig = require('./webpack.config')(config);
 
 gulp.task('scripts', (done) => {
-  webpack(webpackConfig, (err, stats) => {
-    if (err) {
-      gutil.log(gutil.colors.red(err()));
-    }
-    const result = stats.toJson();
-    if (result.errors.length) {
-      result.errors.forEach((error) => {
-        gutil.log(gutil.colors.red(error));
-      });
-    }
-    done();
-  });
+    webpack(webpackConfig, (err, stats) => {
+        if (err) {
+            gutil.log(gutil.colors.red(err()));
+        }
+        const result = stats.toJson();
+        if (result.errors.length) {
+            result.errors.forEach((error) => {
+                gutil.log(gutil.colors.red(error));
+            });
+        }
+        done();
+    });
 });
 
 
 // images
 gulp.task('images', ['favicon'], () => {
-  return gulp.src(config.images.toolkit.src)
-    .pipe(imagemin())
-    .pipe(gulp.dest(config.images.toolkit.dest));
+    return gulp.src(config.images.toolkit.src)
+        .pipe(imagemin())
+        .pipe(gulp.dest(config.images.toolkit.dest));
 });
 
 gulp.task('favicon', () => {
-  return gulp.src('src/favicon.ico')
-  .pipe(gulp.dest(config.dest));
+    return gulp.src('src/favicon.ico')
+        .pipe(gulp.dest(config.dest));
 });
 
 // fonts
 gulp.task('fonts', () => {
-  return gulp.src(config.fonts.indico.src)
-    .pipe(gulp.dest(config.fonts.indico.dest));
+    return gulp.src(config.fonts.indico.src)
+        .pipe(gulp.dest(config.fonts.indico.dest));
 });
 
 // assembler
 gulp.task('assembler', (done) => {
-  assembler({
-    logErrors: config.dev,
-    dest: config.dest,
-  });
-  done();
+    assembler({
+        logErrors: config.dev,
+        dest: config.dest,
+    });
+    done();
 });
 
 
 // server
 gulp.task('serve', () => {
 
-  browserSync({
-    server: {
-      baseDir: config.dest,
-    },
-    notify: false,
-    logPrefix: 'FABRICATOR',
-  });
+    browserSync({
+        server: {
+            baseDir: config.dest,
+        },
+        notify: false,
+        logPrefix: 'FABRICATOR',
+    });
 
-  gulp.task('assembler:watch', ['assembler'], browserSync.reload);
-  gulp.watch(config.templates.watch, ['assembler:watch']);
+    gulp.task('assembler:watch', ['assembler'], browserSync.reload);
+    gulp.watch(config.templates.watch, ['assembler:watch']);
 
-  gulp.task('styles:watch', ['styles']);
-  gulp.watch([config.styles.fabricator.watch, config.styles.toolkit.watch], ['styles:watch']);
+    gulp.task('styles:watch', ['styles']);
+    gulp.watch([config.styles.fabricator.watch, config.styles.toolkit.watch], ['styles:watch']);
 
-  gulp.task('scripts:watch', ['scripts'], browserSync.reload);
-  gulp.watch([config.scripts.fabricator.watch, config.scripts.toolkit.watch], ['scripts:watch']);
+    gulp.task('scripts:watch', ['scripts'], browserSync.reload);
+    gulp.watch([config.scripts.fabricator.watch, config.scripts.toolkit.watch], ['scripts:watch']);
 
-  gulp.task('images:watch', ['images'], browserSync.reload);
-  gulp.watch(config.images.toolkit.watch, ['images:watch']);
+    gulp.task('images:watch', ['images'], browserSync.reload);
+    gulp.watch(config.images.toolkit.watch, ['images:watch']);
 
-  gulp.task('fonts:watch', ['fonts'], browserSync.reload);
-  gulp.watch(config.fonts.indico.watch, ['fonts:watch']);
+    gulp.task('fonts:watch', ['fonts'], browserSync.reload);
+    gulp.watch(config.fonts.indico.watch, ['fonts:watch']);
 
 });
 
@@ -201,20 +201,20 @@ gulp.task('serve', () => {
 gulp.task('default', ['clean'], () => {
 
   // define build tasks
-  const tasks = [
-    'styles',
-    'scripts',
-    'images',
-    'fonts',
-    'assembler',
-  ];
+    const tasks = [
+        'styles',
+        'scripts',
+        'images',
+        'fonts',
+        'assembler',
+    ];
 
   // run build
-  runSequence(tasks, () => {
-    if (config.dev) {
-      gulp.start('serve');
-    }
-  });
+    runSequence(tasks, () => {
+        if (config.dev) {
+            gulp.start('serve');
+        }
+    });
 
 });
 
